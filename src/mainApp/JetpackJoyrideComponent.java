49e663b897d86e458e05255d3575ad28ff1fd0b0
ramsdemc <ramsdemc@rose-hulman.edu>
2024-01-30 11:46:24 -0500

    started paintComponent function

--- src/mainApp/JetpackJoyrideComponent.java
+++ src/mainApp/JetpackJoyrideComponent.java
@@ -1,5 +1,7 @@
 package mainApp;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -55,6 +57,14 @@ public class JetpackJoyrideComponent extends JComponent{
 		} catch (FileNotFoundException e) {
 			System.err.println("File Not Found: " + filename);
 			e.printStackTrace();
+		}
+	}
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		for(Collidable c : collidables) {
+			c.drawOn(g2);
 		}
 	}
 }
