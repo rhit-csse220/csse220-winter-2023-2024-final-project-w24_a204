8ce17c83bc27852d5f4ad7f065321bf643fcbe3b
zhango <zhango@137.112.214.249>
2024-02-05 20:20:47 -0500

    added a shouldRemove boolean to Collidable and methods to use it

--- src/mainApp/Collidable.java
+++ src/mainApp/Collidable.java
@@ -6,6 +6,7 @@ public abstract class Collidable {
 	
 	protected double xPos;
 	protected double yPos;
+	private boolean shouldRemove;
 	
 	public Collidable(int xPos, int yPos) {
 		this.xPos = xPos;
@@ -14,4 +15,11 @@ public abstract class Collidable {
 	
 	public abstract void drawOn(Graphics g);
 	
+	public boolean shouldRemove() {
+		return this.shouldRemove;
+	}
+	
+	public void markToRemove() {
+		this.shouldRemove = true;
+	}
 }
